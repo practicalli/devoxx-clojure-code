becc3b8cbacb037f5ab109c1fab3c0de07ec3c3b
John Stevenson <john@jr0cket.co.uk>
2015-06-03 14:40:44 +0100

    Clojure project created with Leiningen default template

---
+++ test/salary_calculator/core_test.clj
@@ -0,0 +1,7 @@
+(ns salary-calculator.core-test
+  (:require [clojure.test :refer :all]
+            [salary-calculator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
